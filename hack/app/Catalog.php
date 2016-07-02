d27d53e4794ed00c183e66dc04e418eee7508280
aki323buri2 <aki323buri2@gmail.com>
2016-07-02 14:38:40 +0900

    catalog model and migration

---
+++ hack/app/Catalog.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Catalog extends Model
+{
+	protected $table = 'catalog';
+	protected $primaryKey = 'catno';
+
+	protected $columns = [];
+
+    public function __construct()
+    {
+
+    	$values = [
+			['catno'  , 'string'        , 'text'    , 'カタログＣＤ'], 
+			['shcds'  , 'string'        , 'text'    , 'ｼｮｸﾘｭｰＣＤ'], 
+			['eoscd'  , 'string'        , 'text'    , 'ＥＯＳＣＤ'], 
+			['mekame' , 'string'        , 'text'    , 'メーカー名'], 
+			['shiren' , 'string'        , 'text'    , '仕入先ＣＤ'], 
+			['hinmei' , 'string'        , 'text'    , '品名'], 
+			['sanchi' , 'string'        , 'text'    , '産地'], 
+			['tenyou' , 'string'        , 'text'    , '天・養'], 
+			['nouka'  , 'decimal(10,2)' , 'numeric' , '納価'], 
+			['baika'  , 'decimal(10,2)' , 'numeric' , '売価'], 
+			['stanka' , 'decimal(10,2)' , 'numeric' , '仕入'], 
+		];
+		$names = [
+			'name', 
+			'migrate', 
+			'hot', 
+			'title', 
+		];
+		foreach ($values as $value)
+		{
+			$columns[] = (object)array_combine($names, $value);
+		}
+		$this->columns = collect($columns)->keyBy('name');
+    }
+}
--- hack/app/Http/Controllers/CatalogController.php
+++ hack/app/Http/Controllers/CatalogController.php
@@ -7,8 +7,15 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use \Route;
 
+use App\Catalog;
+
 class CatalogController extends Controller
 {
+	protected $catalog;
+	public function __construct(Catalog $catalog)
+	{
+		$this->catalog = $catalog;
+	}
     static public function route()
     {
     	Route::get('/catalog', 'CatalogController@index');
